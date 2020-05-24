e0572f438f5b978abbbef37e3e96ef15bb469191
Jay Gibran <jaygibran@gmail.com>
2020-05-24 15:03:31 +0100

    frontend: isolating components

--- frontend/src/pages/SignIn/styles.ts
+++ frontend/src/pages/SignIn/styles.ts
@@ -26,40 +26,6 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    input{
-      background: #232129;
-      color: #f4ede8;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      padding: 16px;
-      width: 100%;
-
-      &::placeholder {
-        color: 666360;
-      }
-
-      & + input {
-        margin-top: 8px;
-      }
-    }
-
-    button{
-      background: #ff9000;
-      height: 56px;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      width: 100%;
-      color: #3112e38;
-      font-weight: 500;
-      margin-top: 16px;
-      transition: background-color 0.2s;
-
-      &:hover{
-        background: ${shade(0.2, '#ff9000')}
-      }
-    }
-
     a {
       color: #f4ede8;
       display: block;
@@ -68,7 +34,7 @@ export const Content = styled.div`
       transition: color 0.2s;
 
       &:hover {
-        color: ${shade(0.2, '#f4ede8')}
+        color: ${shade(0.2, '#f4ede8')};
       }
     }
   }
@@ -82,14 +48,13 @@ export const Content = styled.div`
     display: flex;
     align-items: center;
 
-    &:hover{
-      color: ${shade(0.2, '#ff9000')}
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
     }
 
     svg {
       margin-right: 16px;
     }
-
   }
 `;
 
